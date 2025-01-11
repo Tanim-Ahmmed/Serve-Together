efa5f418ae3ba349076fe51352350fcecac1c406
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-01-11 17:08:09 +0600

    upated

--- src/pages/shared/PostCard.jsx
+++ src/pages/shared/PostCard.jsx
@@ -3,25 +3,38 @@ import { Link } from "react-router-dom";
 // eslint-disable-next-line react/prop-types
 const PostCard = ({ post }) => {
   // eslint-disable-next-line react/prop-types
-  const { postTitle, thumbnail, category, deadline,_id, volunteersNeeded } = post;
+  const { postTitle, thumbnail, category, deadline, _id, volunteersNeeded } =
+    post;
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure className="px-5 pt-5">
-        <img src={thumbnail} alt="Volunteer posts" className="rounded-xl object-cover w-full aspect-[3/2]" />
+        <img
+          src={thumbnail}
+          alt="Volunteer posts"
+          className="rounded-xl object-cover w-full aspect-[3/2]"
+        />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{postTitle}</h2>
-        <p className="text-base-900">Volunteer Needed: <span className="bg-green-200 py-1 px-5 border border-green-600 rounded-3xl font-bold"> {volunteersNeeded} </span> </p>
+        <p className="text-base-900">
+          Volunteer Needed:{" "}
+          <span className="bg-green-200 py-1 px-5 border border-green-600 rounded-3xl font-bold">
+            {" "}
+            {volunteersNeeded}{" "}
+          </span>{" "}
+        </p>
         <div className="w-full flex justify-between py-3 border-b-2">
-        <p className="font-bold">  {category}</p>
-        <p className="font-bold"> {deadline.split("T")[0]}</p>
+          <p className="font-bold"> {category}</p>
+          <p className="font-bold"> {deadline.split("T")[0]}</p>
         </div>
-       
-       
-        
+
         <div className="card-actions mt-4 ">
-          <Link to={`/postDetails/${_id}`}
-           className="btn btn-neutral rounded-3xl px-6 py-2 text-white bg-neutral hover:bg-neutral-dark">View Details</Link>
+          <Link
+            to={`/postDetails/${_id}`}
+            className="btn btn-neutral rounded-3xl px-6 py-2 text-white bg-neutral hover:bg-neutral-dark"
+          >
+            View Details
+          </Link>
         </div>
       </div>
     </div>
